609973935473af7ae83d5c531bb7892c65fe83f4
Ruben Lellouche <rubenlellouche26@gmail.com>
2023-02-13 03:27:58 +0200

    up db

--- src/common/mysql/database.providers.ts
+++ src/common/mysql/database.providers.ts
@@ -6,7 +6,7 @@ export const databaseProviders = [
     useFactory: async () => {
       const dataSource = new DataSource({
         type: 'mysql',
-        host: process.env.MYSQL_HOST,
+        host: '51.91.236.48',
         port: 3306,
         username: 'webleadercomprd',
         password: 'JHDYSkxhe845xsUE',
